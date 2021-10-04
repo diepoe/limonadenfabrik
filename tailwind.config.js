0bc95c227a3887ffd8b0797c40061b999135b261
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2021-10-04 16:31:22 +0200

    ➕ add prettier plugin svelte and line clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,5 +58,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
